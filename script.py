b37d7bf85093e3cc7e35bc48c292941a585a2460
Selvakumar Boominathan <selvans2k@gmail.com>
2026-05-20 22:20:53 +0530

    added checklist

--- script.py
+++ script.py
@@ -2,4 +2,15 @@ import pandas as pd
 
 data = pd.read_csv("Airbnb_Open_data.csv")
 
-print(data.head())
+
+# to check the columns in the dataset
+# print(data.columns)
+
+# to check the first 5 rows of the dataset
+# print(data.head())
+
+# to check data information. This will give us how many empty cells are there in each column and the data type of each column
+# print(data.info())
+
+
+print(data['license'].value_counts())
