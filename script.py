ebdd886037411abd580765a996f5f459c3fcea92
Selvakumar Boominathan <selvans2k@gmail.com>
2026-05-21 00:05:01 +0530

    delete and renaming columns done

--- script.py
+++ script.py
@@ -12,5 +12,36 @@ data = pd.read_csv("Airbnb_Open_data.csv")
 # to check data information. This will give us how many empty cells are there in each column and the data type of each column
 # print(data.info())
 
+# Removing the columns
+# print(data.columns)
 
-print(data['license'].value_counts())
+
+columns_to_keep = ['NAME', 'host id', 'host_identity_verified', 'host name',
+                   'neighbourhood group', 'neighbourhood', 'lat', 'long', 'country',
+                   'country code', 'instant_bookable', 'cancellation_policy', 'room type',
+                   'Construction year', 'price', 'service fee', 'minimum nights',
+                   'number of reviews', 'last review']
+
+columns_to_remove = ['id', 'reviews per month',
+                     'review rate number', 'calculated host listings count',
+                     'availability 365', 'house_rules', 'license']
+
+# to check the number of columns to keep and remove
+# print(len(columns_to_keep))
+# print(len(columns_to_remove))
+
+
+# Deleting redundant columns
+newData = data.drop(columns=columns_to_remove)
+
+# print(newData.tail())
+
+
+# Renaming of columns
+
+print(data.columns)
+
+data.rename(columns={"NAME": "Name",
+            'host_identity_verified': 'host_id_verified'}, inplace=True)
+
+print(data.columns)
